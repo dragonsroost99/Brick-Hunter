4cd20e6cf819286d164e4df6c9d45e079dc07d3f
dragonsroost99 <dragonsroost99@gmail.com>
2026-04-20 21:11:05 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'brick-hunter-v1';
+const SHELL = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;700;900&family=Barlow:wght@400;500;600&display=swap',
+  'https://unpkg.com/react@17/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  // Network first for API calls, cache first for everything else
+  if (e.request.url.includes('rebrickable.com') || e.request.url.includes('corsproxy.io') || e.request.url.includes('img.bricklink.com')) {
+    e.respondWith(fetch(e.request).catch(() => new Response('', {status: 503})));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
